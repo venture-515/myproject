f4e0456aee4678cc6f77a24c4c7b26aac025887e
faizan <faizanbutt5152@gamil.com>
2026-05-15 16:53:32 +0500

    my changes

---
+++ app/news/latest/page.jsx
@@ -0,0 +1,8 @@
+
+export default function LatestNews() {
+  return (
+    <main>
+      <h2> News latest </h2>
+    </main>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h2> DashBoard </h2>
+    </main>
+  )
+}
